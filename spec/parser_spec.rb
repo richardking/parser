601e84b748cf1c313a9d08a1142ff0e876680f6e
Richard King <rkingucla@ymail.com>
2014-06-24 14:03:17 -0700

    add tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,11 +1,27 @@
 require 'helper'
 
+def net_transactions_for(accounts, routing_number, account_number)
+  accounts.select{|a| a['routing_number'] == routing_number && a['account_number'] == account_number }.first['net_transactions']
+end
+
 describe Parser do
   subject { JSON.parse(Parser.new('spec/fixtures/valid_data.bdi').build_json) }
 
   its(['batch']) { should eq('99') }
   its(['description']) { should eq('Payroll for January') }
   its('size') { should eq(3) }
+
+  context 'account net balances' do
+    let(:accounts_info) { JSON.parse(Parser.new('spec/fixtures/valid_data.bdi').build_json)['accounts'] }
+
+    it 'should calculate the correct balance for acct 1' do
+      expect(net_transactions_for(accounts_info, '111222333', '9991')).to eq(-390100)
+    end
+
+    it 'should calculate the correct balance for acct 2' do
+      expect(net_transactions_for(accounts_info, '123456789', '55550')).to eq(380100)
+    end
+  end
 
   context 'invalid file type' do
     it 'should raise error' do
